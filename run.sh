3c3e8a0876c7e4a0a6c7519fff63196b191b2ce9
Nunzio Barone <nunzio03@hotmail.it>
2025-06-28 08:45:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 xhost +
-docker run -it --rm --net host --ipc host \
+docker run -it --rm --net host --ipc host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
